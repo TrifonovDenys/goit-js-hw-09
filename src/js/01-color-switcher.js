d51fc8e270c55e7e3d5678c364cab05bf1bd0876
Trifonov Denys <trifonowden123@gmail.com>
2023-05-08 22:04:59 +0300

    exersise 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.body
+const buttonStart = document.querySelector('[data-start]')
+const buttonStop = document.querySelector('[data-stop]')
+let timerId = null;
+
+buttonStart.addEventListener('click', startColorChange)
+buttonStop.addEventListener('click', stopColorChange)
+
+function startColorChange() {
+  body.style.backgroundColor = getRandomHexColor()
+  buttonStart.disabled = true
+  buttonStop.disabled = false
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor()
+   }, 1000)
+}
+
+function stopColorChange() {
+  clearInterval(timerId)
+  buttonStart.disabled = false
+  buttonStop.disabled = true
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
