bc052f41575a4c3cf785812556ad8d70d01fcbd4
Trifonov Denys <trifonowden123@gmail.com>
2023-05-13 22:49:05 +0300

    exersise 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,25 +1,53 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+function createPromise(i, promiseDelay) {
   const shouldResolve = Math.random() > 0.3;
   const promise = new Promise((resolve, reject) => {
-    setInterval(() => {
+    setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve({ i, promiseDelay });
       } else {
-        reject({ position, delay });
+        reject({ i, promiseDelay });
       }
-    }, delay);
+    }, promiseDelay);
   });
   return promise;
 }
 
-createPromise(2, 1000)
-  .then(({ position, delay }) => {
-      setTimeout(() => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);      
-      },delay)  
-  })
+const refs = {
+  buttn: document.querySelector('button'),
+  inputFirstDelay: document.querySelector('[name="delay"]'),
+  inputDelayStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
+};
 
+refs.buttn.addEventListener('click', onButtonClick);
 
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+function onButtonClick(e) {
+  e.preventDefault();
+  let promiseDelay = refs.inputFirstDelay.valueAsNumber;
+  const delay = refs.inputDelayStep.valueAsNumber;
+  const a = refs.inputAmount.valueAsNumber;
+
+  const massage = ['Type First delay','Type Delay step','Type Amount']
+  if (refs.inputFirstDelay.value === '') Notiflix.Notify.info(`${massage[0]}`);
+  if (refs.inputDelayStep.value === '') Notiflix.Notify.info(`${massage[1]}`);
+  if (refs.inputAmount.value === '') Notiflix.Notify.info(`${massage[2]}`);
+
+  for (i = 1; i <= a; i++) {
+    createPromise(i, promiseDelay)
+      .then(({ i, promiseDelay }) => {
+        console.log(`✅ Fulfilled promise ${i} in ${promiseDelay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${i} in ${promiseDelay}ms`
+        );
+      })
+      .catch(({ i, promiseDelay }) => {
+        console.log(`❌ Rejected promise ${i} in ${promiseDelay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${i} in ${promiseDelay}ms`
+        );
+      });
+    promiseDelay += delay;
+  }
+}
