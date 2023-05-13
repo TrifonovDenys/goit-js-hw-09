cadb64260fcd783c46693bc921b4ec9c01d587f6
TrifonovDenys <54479637+TrifonovDenys@users.noreply.github.com>
2023-05-13 19:49:28 +0000

    Deploying to gh-pages from @ TrifonovDenys/goit-js-hw-09@bc052f41575a4c3cf785812556ad8d70d01fcbd4 🚀

--- 03-promises.1d8ddf8d.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,n,t;(o=2,n=1e3,t=Math.random()>.3,new Promise((function(i,e){setInterval((function(){t?i({position:o,delay:n}):e({position:o,delay:n})}),n)}))).then((function(o){var n=o.position,t=o.delay;setTimeout((function(){console.log("✅ Fulfilled promise ".concat(n," in ").concat(t,"ms"))}),t)})).catch((function(o){var n=o.position,t=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(t,"ms"))}))}();
-//# sourceMappingURL=03-promises.1d8ddf8d.js.map
